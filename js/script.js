78e65c867634471b13424cbc35baf3c94a153c49
Bagus1997 <103160011+Bagus1997@users.noreply.github.com>
2025-11-07 17:20:55 +0700

    validasi form javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+welcomeMessage();
+
 function welcomeMessage() {
     let name = prompt("Please enter your name:");
-    console.log("Hello, " + name + "Welcome to Bagus Company!");
+
+    if(name !=null) {
+    }
+
+    document.getElementById("welcome-speech").innerHTML = "Hello, " + name  + " Welcome to Bagus Company!";
+    console.log("Hello " + name  + "Welcome to Bagus Company!");
+}
+function validateForm (  ) {
+
 }
